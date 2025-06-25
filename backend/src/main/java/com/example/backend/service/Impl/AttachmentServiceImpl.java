0923059983bc2bc15386712e31aaa570d3147c2c
MengZhi20040707 <2053987493@qq.com>
2025-06-25 15:42:17 +0800

    附件IMAP连接池管理

--- backend/src/main/java/com/example/backend/service/Impl/AttachmentServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/Impl/AttachmentServiceImpl.java
@@ -1,24 +1,59 @@
 package com.example.backend.service.Impl;
 
 import com.example.backend.Client.ImapClient;
+import com.example.backend.Client.Manager.ImapClientManager;
 import com.example.backend.entity.Attachment;
 import com.example.backend.mapper.AttachmentMapper;
 import com.example.backend.service.AttachmentService;
 import com.example.backend.utils.ResultVo;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.annotation.RequestScope;
+
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.UUID;
 
 @Service
+@RequestScope
 public class AttachmentServiceImpl implements AttachmentService {
     @Value("${attachment.storage.dir}") // 从配置文件中读取存储路径
     private String storageDir;
 
+    // 用户邮箱
+    private String userEmail;
+
     @Autowired
+    private ImapClientManager imapClientManager;
+
     private ImapClient imapClient;
+
+    public AttachmentServiceImpl(ImapClientManager imapClientManager,  @Autowired(required = false) HttpServletRequest request) {
+        this.imapClientManager = imapClientManager;
+        userEmail = extractUserEmailFromRequest(request);
+        imapClient = imapClientManager.getClientForUser(userEmail);
+    }
+
+    private String extractUserEmailFromRequest(HttpServletRequest request) {
+        if (request == null) {
+            return null;
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("loginUserEmail".equals(cookie.getName()) && StringUtils.hasText(cookie.getValue())) {
+                    System.out.println(cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
 
     @Override
     public ResultVo getAttachmentById(Long attachmentId) {
